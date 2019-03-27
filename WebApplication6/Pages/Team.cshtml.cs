e58f29b55c22bdec9bf062e5cf184b123dd76cb0
Bart Ziengs <BZI@qdelft.nl>
2019-03-27 14:18:54 +0100

    deleted obsolete files and addes teampage and links

--- WebApplication6/Pages/Team.cshtml.cs
+++ WebApplication6/Pages/Team.cshtml.cs
@@ -13,7 +13,8 @@ namespace WebApplication6.Pages
         public List<TeamMember> TeamMembers = new List<TeamMember>();
         public void OnGet()
         {
-            TeamMembers.Add(new TeamMember
+            TeamMembers.AddRange(new List<TeamMember> {
+            new TeamMember
             {
                 Name = "Alex van der Meijs",
                 Age = 51,
@@ -21,8 +22,8 @@ namespace WebApplication6.Pages
                 GreatestExperience = "Dan kom ik toch uit bij de Maaslandloop wanneer je op zaterdagochtend over een dijk richting Gorinchem aan het hardlopen bent, al ruim 250 km achter de kiezen hebt met elkaar, het zonnetje op komt en je de spreekwoordelijke stal al bijna ruikt. Geweldig gewoon.",
                 Motivation = "Van de 10 voorbije edities heb ik er zelf 5 actief meegemaakt. Toch is de motivatie weer volop aanwezig, omdat het stuk voor stuk mooie edities waren met uiteraard moeilijke momenten maar ook zeer veel hoogtepunte de Maaslandloop naast een individuele ook zeker een teamprestatie is en het blijft geweldig om dat mee te maken; ik twee keer eerder met een volledig nieuw team(op mezelf na) heb deel genomen aan de Maaslandloop en ik dat als super leuk heb ervaren; hardlopen met als doel zo’n 32 keer te vlammen tijdens de Maaslandloop gewoon een goede motivatie is en het daarmee ook een stuk leuker wordt, en het een goed doel steunt waarbij kinderen centraal staan.",
                 ImgSrc = "/alex.jpg",
-            });
-            TeamMembers.Add(new TeamMember
+            },
+            new TeamMember
             {
                 Name = "Raymond Verhoef",
                 Age = 37,
@@ -30,9 +31,19 @@ namespace WebApplication6.Pages
                 GreatestExperience = "Het passeren van het 35km punt tijdens de Marathon van Zeeuws Vlaanderen, waarbij ik mijn zoontje passeerde die me letterlijk een duwtje in de rug gaf om door te gaan.",
                 Motivation = "De Maaslandloop is een waanzinnige sportieve uitdaging, die je samen met een team collega’s moet volbrengen. De combinatie waarbij we ook nog eens een ontzettend goed doel kunnen steunen is een fantastische drijfveer om door te zetten! Een evenement als de Maaslandloop is een prachtige teamprestatie.",
                 ImgSrc = "/raymond.jpg",
+            },
+            new TeamMember
+            {
+                Name = "Piet de Jong",
+                Age = 48,
+                SpareTime = "Al sinds mijn jeugd aan hardlopen. Mijn eerste kennismaking was op 8-jarige leeftijd lid worden van een atletiekvereniging waar ik beslist niet uitblonk in kogelstoten en hoogspringen, maar wel in de langere hardloopafstanden. Verder hou ik deze hobby in balans met mijn gezin, waar helaas geen van mijn kinderen interesse heeft in hardlopen.",
+                GreatestExperience = "De Westland marathon van 2014. Dit evenement werd éénmalig georganiseerd en lopen door je eigen achtertuin daar kan niets tegenop. Het was een mooie hete dag en onderweg hebben mijn kinderen mij voorzien van drinken en de nodige gelletjes. En dan afsluiten met een 7de plaats, mooier gaat het voorlopig niet worden.",
+                Motivation = "Motivatie voor deze loop heb ik eigenlijk niet nodig. Het lopen met collega’s en voor het goede doel voor kinderen maakt meedoen nog makkelijker. ",
+                ImgSrc = "/piet.png",
+            }
             });
         }
-    } 
+    }
 
     public class TeamMember
     {
